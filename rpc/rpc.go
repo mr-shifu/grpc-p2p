37fefde6e964c44f0138744db2d03a8a0c5d4e46
hnamzian <namazian87@gmail.com>
2024-02-09 18:13:00 -0500

    GetPeers returns peer state + Get peer info from cts and adds peer to peerstore

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -2,11 +2,14 @@ package rpc
 
 import (
 	"context"
+	"errors"
 
 	"github.com/mr-shifu/grpc-p2p/peer"
 	p2p_pb "github.com/mr-shifu/grpc-p2p/proto"
 	"github.com/rs/zerolog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+	gpeer "google.golang.org/grpc/peer"
 )
 
 type RpcService struct {
@@ -28,10 +31,31 @@ func (rs *RpcService) RegisterService(s grpc.ServiceRegistrar) {
 }
 
 func (r *RpcService) GetPeers(ctx context.Context, req *p2p_pb.GetPeersRequest) (*p2p_pb.GetPeersResponse, error) {
+	peer, err := getPeerFromContext(ctx)
+	if err != nil {
+		// return nil, errors.New("failed to validate peer")
+	}
+	r.ps.AddPeer(peer)
+
 	peers := r.ps.GetPeers()
 	pbPeers := peersToPbPeers(peers)
 	return &p2p_pb.GetPeersResponse{
 		Peers: pbPeers,
+	}, nil
+}
+
+func getPeerFromContext(ctx context.Context) (*peer.Peer, error) {
+	md, _ := metadata.FromIncomingContext(ctx)
+	cn := md.Get("cluster_name")
+	pn := md.Get("peer_name")
+	p, ok := gpeer.FromContext(ctx)
+	if !ok {
+		return nil, errors.New("peer not found")
+	}
+	return &peer.Peer{
+		Name:        pn[0],
+		ClusterName: cn[0],
+		Addr:        p.LocalAddr.String(),
 	}, nil
 }
 
@@ -42,6 +66,7 @@ func peersToPbPeers(peers []*peer.Peer) []*p2p_pb.Peer {
 			Name:        peer.Name,
 			Address:     peer.Addr,
 			CLusterName: peer.ClusterName,
+			State:       peer.State().String(),
 		}
 		pbPeers = append(pbPeers, p)
 	}
