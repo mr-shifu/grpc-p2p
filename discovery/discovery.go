0cc24680197dc645fcf0edd5fc797ff171c75f5a
hnamzian <namazian87@gmail.com>
2024-02-12 12:00:06 -0500

    Added comments to the functions

--- discovery/discovery.go
+++ discovery/discovery.go
@@ -13,6 +13,7 @@ type Discovery struct {
 	logger zerolog.Logger
 }
 
+// NewDiscovery creates a new discovery service
 func NewDiscovery(ps *peer.PeerService, logger zerolog.Logger) *Discovery {
 	return &Discovery{
 		ps:     ps,
@@ -22,7 +23,7 @@ func NewDiscovery(ps *peer.PeerService, logger zerolog.Logger) *Discovery {
 
 // scan starts discovery
 // 1. Get adjacent peers from peers in the peerstore
-// 2. Adds all adjacent peers to the peerstore
+// 2. Remove duplicate peers
 func (d *Discovery) scan(ctx context.Context) []*peer.Peer {
 	peers := d.ps.GetPeers()
 
@@ -45,22 +46,29 @@ func (d *Discovery) refresh(ctx context.Context, peers []*peer.Peer) error {
 	for _, p := range peers {
 		if p.GetState() != peer.Ready {
 			// connect to the peer and add to peerstore
-			if _, err := d.ps.Connect(p); err != nil {
-				continue
-			}
+			d.ps.Connect(p)
 		}
 	}
 	return nil
 }
 
+// Start starts peer discovery
+// 1. Scans all peers in the peerstore to get adjacent peers
+// 2. Adds all adjacent peers to the peerstore
+// 3. Refreshes peers' connections
 func (d *Discovery) Start(ctx context.Context) error {
 	go func() {
 		for {
+			// scan all peers in the peerstore to get adjacent peers
 			peers := d.scan(ctx)
+			// update peersetore with adjacent peers
 			d.ps.AddPeers(peers)
+			// refresh peers' connections
 			if err := d.refresh(ctx, peers); err != nil {
 				d.logger.Error().Err(err).Msg("failed to refresh peers")
 			}
+			// ToDo - make this configurable
+			// sleep for 1 second
 			time.Sleep(1 * time.Second)
 		}
 	}()
